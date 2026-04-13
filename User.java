d8cae8475a84994c9ec884e0140bb6dead400164
nurdauletsteamakk077-png <nurdauletsteamakk077@gmail.com>
2026-04-13 21:56:51 +0500

    Create User.java

---
+++ User.java
@@ -0,0 +1,13 @@
+abstract class User {
+    protected int id;
+    protected String name;
+    protected String email;
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public abstract void login();
+}
