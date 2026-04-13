99f5fd46401bbfd79220cce5b307c2b771b460a2
nurdauletsteamakk077-png <nurdauletsteamakk077@gmail.com>
2026-04-13 22:07:49 +0500

    Create Grade.java

---
+++ Grade.java
@@ -0,0 +1,9 @@
+class Grade {
+    private Student student;
+    private int value;
+
+    public Grade(Student student, int value) {
+        this.student = student;
+        this.value = value;
+    }
+}
